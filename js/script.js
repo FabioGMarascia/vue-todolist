d06169abbaf4b476351bdb191bcbd1391f4ddbf5
FabioGMarascia <fabiogaspare.marascia@gmail.com>
2024-05-14 15:32:46 +0200

    M-0 list

--- js/script.js
+++ js/script.js
@@ -3,7 +3,14 @@ const { createApp } = Vue;
 createApp({
 	data() {
 		return {
-			message: "Hello Vue!",
+			toDoList: [
+				`Fare la spesa`,
+				`Guardare la partita`,
+				`Pulizia della camera`,
+				`Preparare la cena`,
+			],
+			myH1Style: `text-center fw-bold display-3 text-warning my-4`,
+			myLiStyle: `fw-bold fs-5`,
 		};
 	},
 }).mount("#app");
