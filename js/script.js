e36a810b52613ef8446eb412bd22fad142105c1a
FabioGMarascia <fabiogaspare.marascia@gmail.com>
2024-05-14 16:21:35 +0200

    M-2 delete list element

--- js/script.js
+++ js/script.js
@@ -11,13 +11,17 @@ createApp({
 			],
 			addToDo: ``,
 			myH1Style: `text-center fw-bold display-3 text-warning my-4`,
-			myLiStyle: `fw-bold fs-5`,
+			myLiStyle: `fw-bold fs-5 text-success list-group-item `,
+			myIconStyle: `d-inline-block position-absolute end-0 mt-1 me-2 text-danger fs-4`,
 		};
 	},
 	methods: {
 		addItem() {
 			this.toDoList.push(this.addToDo);
 			this.addToDo = ``;
+		},
+		deleteItem(index) {
+			this.toDoList.splice(index, 1);
 		},
 	},
 }).mount("#app");
