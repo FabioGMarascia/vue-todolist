5ebe3c8bc82d489cd1e7b20ebafd9794216e40c1
FabioGMarascia <fabiogaspare.marascia@gmail.com>
2024-05-14 18:10:50 +0200

    B-1

--- js/script.js
+++ js/script.js
@@ -1,27 +1,53 @@
 const { createApp } = Vue;
-
 createApp({
 	data() {
 		return {
 			toDoList: [
-				`Fare la spesa`,
-				`Guardare la partita`,
-				`Pulizia della camera`,
-				`Preparare la cena`,
+				{
+					string: `Fare la spesa`,
+					done: true,
+				},
+				{
+					string: `Guardare la partita`,
+					done: true,
+				},
+				{
+					string: `Pulizia della camera`,
+					done: true,
+				},
+				{
+					string: `Preparare la cena`,
+					done: true,
+				},
 			],
-			addToDo: ``,
+			input: ``,
 			myH1Style: `text-center fw-bold display-3 text-warning my-4`,
-			myLiStyle: `fw-bold fs-5 text-success list-group-item `,
+			myLiStyle: `fw-bold fs-5 list-group-item`,
 			myIconStyle: `d-inline-block position-absolute end-0 mt-1 me-2 text-danger fs-4`,
+			taskDone: `text-success`,
+			ongoingTask: `text-danger`,
 		};
 	},
 	methods: {
-		addItem() {
-			this.toDoList.push(this.addToDo);
-			this.addToDo = ``;
+		addItem(text) {
+			if (!this.input == ``) {
+				this.toDoList.push({
+					string: text,
+					done: false,
+				});
+				this.input = ``;
+			}
 		},
 		deleteItem(index) {
 			this.toDoList.splice(index, 1);
 		},
+		done(element) {
+			if (element.done == true) {
+				return this.taskDone;
+			} else {
+				return this.ongoingTask;
+			}
+		},
 	},
+	mounted() {},
 }).mount("#app");
