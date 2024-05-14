da0b090029a5218014b72c72c420474c205fc737
FabioGMarascia <fabiogaspare.marascia@gmail.com>
2024-05-14 15:43:16 +0200

    M-1 Input and button

--- js/script.js
+++ js/script.js
@@ -9,8 +9,15 @@ createApp({
 				`Pulizia della camera`,
 				`Preparare la cena`,
 			],
+			addToDo: ``,
 			myH1Style: `text-center fw-bold display-3 text-warning my-4`,
 			myLiStyle: `fw-bold fs-5`,
 		};
+	},
+	methods: {
+		addItem() {
+			this.toDoList.push(this.addToDo);
+			this.addToDo = ``;
+		},
 	},
 }).mount("#app");
